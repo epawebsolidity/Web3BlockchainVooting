438c925c12c563d6a1a330d04f5ae3bc39eea595
Epa Fe <epaweb3js@gmail.com>
2025-12-11 16:00:44 +0700

    first commit

--- my-app/app/providers.tsx
+++ my-app/app/providers.tsx
@@ -14,11 +14,7 @@ import { mainnet, sepolia } from 'wagmi/chains';
 const config = getDefaultConfig({
     appName: 'My RainbowKit App',
     projectId: 'YOUR_PROJECT_ID',
-    chains: [mainnet, sepolia],
-    transports: {
-        [mainnet.id]: http('https://eth-mainnet.g.alchemy.com/v2/...'),
-        [sepolia.id]: http('https://eth-sepolia.g.alchemy.com/v2/...'),
-    },
+    chains: [mainnet, sepolia]
 });
 
 const queryClient = new QueryClient();
